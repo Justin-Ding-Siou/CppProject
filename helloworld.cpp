21b7df729c93662b10b0eb030315bf936b94aea3
Justin-Ding-Siou <69789527+Justin-Ding-Siou@users.noreply.github.com>
2021-09-07 02:57:11 +0800

    454

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+
+    cout << "n's value:" << n << endl
+         << "n's Address中文上傳:" << &n << endl;
+
+    return 0;
+}
